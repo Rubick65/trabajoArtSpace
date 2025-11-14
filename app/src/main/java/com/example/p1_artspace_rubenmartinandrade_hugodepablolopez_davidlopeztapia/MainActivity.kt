b5fac886f362e399cb10443c9ee2e6460001d544
Hugo de Pablo <hugdpalo@gmail.com>
2025-11-14 12:16:36 +0100

    Implementadas funciones para crear titulo,imagen y la interfaz.
    Falta a parte de las funciones que quedan, hacer que la imagen y el texto cambien de tamaño dependiendo del tamaño de la pantalla donde se ejecute.

--- app/src/main/java/com/example/p1_artspace_rubenmartinandrade_hugodepablolopez_davidlopeztapia/MainActivity.kt
+++ app/src/main/java/com/example/p1_artspace_rubenmartinandrade_hugodepablolopez_davidlopeztapia/MainActivity.kt
@@ -7,13 +7,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -24,8 +29,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,7 +47,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             P1_ArtSpace_RubenMartinAndrade_HugodePabloLopez_DavidLopezTapiaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                  //
+                    crearInterfaz(Modifier.padding(innerPadding))
                 }
             }
         }
@@ -48,42 +56,56 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun crearTitulo(@StringRes titulo: Int, modifier: Modifier = Modifier) {
+    Text(
+        text = stringResource(titulo),
+        fontSize = 30.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color.Black.copy(alpha = 1f), //100% de opacidad
+        fontFamily = FontFamily.Cursive,
+        modifier = modifier
+    )
+}
+
+
+@Composable
+fun crearImagen(@DrawableRes imagen: Int, modifier: Modifier = Modifier) {
     Surface(
         modifier = modifier
-    ) {
-        Row(
-            verticalAlignment = Alignment.Top,
-            modifier = modifier.padding(top = 30.dp)
-        ) {
-            Text(
-                text = stringResource(titulo),
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
+            .border(width = 1.dp, color = Color.Gray)
+            .shadow(elevation = 10.dp, shape = RoundedCornerShape(bottomEnd = 16.dp), clip = false),
+    )
+    {
+        Box(
+            modifier = Modifier
+                .height(500.dp)
+                .width(350.dp)
+        )
+        {
+            Image(
+                painter = painterResource(imagen),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(400.dp)
+                    .align(Alignment.Center)
             )
         }
     }
 }
 
 @Composable
-fun crearImagen(@DrawableRes imagen: Int) {
-    Image(
-        painter = painterResource(imagen),
-        contentDescription = null
-    )
-}
-
-@Composable
-fun crearInterfaz() {
-    var titulo by remember { mutableStateOf((R.string.holiwi)) }
-    var imagen by remember { mutableStateOf(R.drawable.gunter)}
-    Column(
+fun crearInterfaz(modifier: Modifier = Modifier) {
+    var titulo by remember { mutableStateOf(R.string.gunter_Titulo) }
+    var imagen by remember { mutableStateOf(R.drawable.gunter) }
+    Box(
         modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-        )
+    )
     {
-        crearTitulo(titulo)
-        Spacer(modifier = Modifier.height(40.dp))
-        crearImagen(imagen)
+        crearTitulo(
+            titulo, Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 40.dp)
+        )
+        crearImagen(imagen, Modifier.align(Alignment.Center))
     }
 }
 
