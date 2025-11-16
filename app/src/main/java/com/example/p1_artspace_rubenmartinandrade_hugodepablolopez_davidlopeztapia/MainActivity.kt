89e02ec8560bdddd2b4e79c4be7369adbf0b89cd
Rubén Martín <martinandraderuben@gmail.com>
2025-11-16 16:20:16 +0100

    casi terminado falta comentar y revisar

--- app/src/main/java/com/example/p1_artspace_rubenmartinandrade_hugodepablolopez_davidlopeztapia/MainActivity.kt
+++ app/src/main/java/com/example/p1_artspace_rubenmartinandrade_hugodepablolopez_davidlopeztapia/MainActivity.kt
@@ -10,13 +10,17 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -33,6 +37,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -42,9 +47,7 @@ import com.example.p1_artspace_rubenmartinandrade_hugodepablolopez_davidlopeztap
 //INICIO (David)
 //Modelo para cada obra
 data class Obra(
-    @StringRes val titulo: Int,
-    @StringRes val artista: Int,
-    @DrawableRes val imagen: Int
+    @StringRes val titulo: Int, @StringRes val artista: Int, @DrawableRes val imagen: Int
 )
 
 //Lista de obras
@@ -64,18 +67,34 @@ class MainActivity : ComponentActivity() {
         setContent {
             P1_ArtSpace_RubenMartinAndrade_HugodePabloLopez_DavidLopezTapiaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    crearInterfaz(Modifier.padding(innerPadding))
+                    CrearInterfaz(Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
+
+// Parámetros de la imagen
+data class ImagenParams(
+    val boxWidth: Int, val boxHeight: Int, val imageSize: Int, val imagePadding: Int
+)
+
+// Parámetros de la información de la obra
+data class InfoParams(
+    val columnWidth: Int,
+    val columnHeight: Int,
+    val textSize1: Int,
+    val textSize2: Int,
+    val buttonWidth: Int,
+    val buttonHeight: Int
+)
+
 @Composable
-fun crearTitulo(@StringRes titulo: Int, modifier: Modifier = Modifier) {
+fun CrearTitulo(@StringRes titulo: Int, modifier: Modifier = Modifier, textSize: Int) {
     Text(
         text = stringResource(titulo),
-        fontSize = 30.sp,
+        fontSize = textSize.sp,
         fontWeight = FontWeight.Bold,
         color = Color.Black.copy(alpha = 1f),
         fontFamily = FontFamily.Cursive,
@@ -84,22 +103,28 @@ fun crearTitulo(@StringRes titulo: Int, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun crearImagen(@DrawableRes imagen: Int, modifier: Modifier = Modifier) {
+fun CrearImagen(
+    @DrawableRes imagen: Int,
+    modifier: Modifier = Modifier,
+    boxWidth: Int,
+    boxHeight: Int,
+    imageSize: Int,
+    imagePadding: Int
+) {
     Surface(
         modifier = modifier
+            .padding(bottom = imagePadding.dp)
             .border(width = 1.dp, color = Color.Gray)
             .shadow(elevation = 10.dp, shape = RoundedCornerShape(bottomEnd = 16.dp), clip = false),
     ) {
         Box(
-            modifier = Modifier
-                .size(width = 350.dp, height = 500.dp)
-        )
-        {
+            modifier = Modifier.size(width = boxWidth.dp, height = boxHeight.dp)
+        ) {
             Image(
                 painter = painterResource(imagen),
                 contentDescription = null,
                 modifier = Modifier
-                    .size(400.dp)
+                    .size(imageSize.dp)
                     .align(Alignment.Center)
             )
         }
@@ -107,79 +132,200 @@ fun crearImagen(@DrawableRes imagen: Int, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun crearInterfaz(modifier: Modifier = Modifier) {
+fun CrearInterfaz(modifier: Modifier = Modifier) {
 
     //INICIO (David)
     var index by remember { mutableStateOf(0) }
     val obraActual = obras[index]
     //FIN (David)
 
-    Box(
+    BoxWithConstraints(
         modifier = Modifier.fillMaxSize(),
     ) {
-        // Título
-        crearTitulo(
+
+        val ancho = maxWidth
+
+
+        // En función del ancho de la pantalla cambiamos el tamaño de los textos
+        val textSizeTitulo = when {
+            ancho < 600.dp -> 40
+            ancho < 840.dp -> 50
+            else -> 40
+        }
+
+        // Parámetros de la imagen
+        val imagenParams = when {
+            ancho < 400.dp -> ImagenParams(200, 350, 400, 100)
+            ancho < 600.dp -> ImagenParams(350, 500, 400, 100)
+            ancho < 840.dp -> ImagenParams(620, 800, 700, 200)
+            else -> ImagenParams(300, 450, 350, 100)
+        }
+
+        // Parámetros de la información de la obra
+        val infoParams = when {
+            ancho < 600.dp -> InfoParams(280, 110, 20, 22, 150, 40)
+            ancho < 840.dp -> InfoParams(380, 180, 30, 32, 200, 60)
+            else -> InfoParams(280, 110, 20, 22, 150, 45)
+        }
+
+
+
+        CrearTitulo(
             obraActual.titulo,
             Modifier
                 .align(Alignment.TopCenter)
-                .padding(top = 40.dp)
+                .padding(top = 40.dp),
+            textSize = textSizeTitulo
         )
 
-        // Imagen
-        crearImagen(
+        CrearImagen(
             obraActual.imagen,
-            Modifier.align(Alignment.Center)
+            Modifier.align(Alignment.Center),
+            boxWidth = imagenParams.boxWidth,
+            boxHeight = imagenParams.boxHeight,
+            imageSize = imagenParams.imageSize,
+            imagePadding = imagenParams.imagePadding
         )
 
-        //INICIO_2 (David)
-        info_obra(
+        InfoObra(
             obra = obraActual,
             index = index,
             modifier = Modifier
                 .align(Alignment.BottomCenter)
-                .padding(bottom = 40.dp) // separación del borde inferior
+                .padding(bottom = 40.dp),
+            columnWith = infoParams.columnWidth,
+            columnHeight = infoParams.columnHeight,
+            texSize1 = infoParams.textSize1,
+            textSize2 = infoParams.textSize2,
+            buttonWidth = infoParams.buttonWidth,
+            buttonHeight = infoParams.buttonHeight,
+            onClickNext = {
+                index = if (index < 3) index + 1 else 0
+            },
+            onClickPrevious = {
+                index = if (index > 0) index - 1 else 3
+            }
         )
-        //FIN_2 (David)
+
+
     }
+
+
+    //FIN_2 (David)
 }
+
 
 //INICIO_2 (David)
 @Composable
-fun info_obra(obra: Obra, index: Int, modifier: Modifier = Modifier) {
-    Surface(
-        color = Color(0xFFF5F5F5), // gris clarito casi blanco
-        shape = RoundedCornerShape(12.dp),
-        shadowElevation = 4.dp,
-        modifier = modifier
-            .padding(horizontal = 16.dp)
+fun InfoObra(
+    obra: Obra,
+    index: Int,
+    modifier: Modifier = Modifier,
+    columnWith: Int,
+    columnHeight: Int,
+    texSize1: Int,
+    textSize2: Int,
+    buttonWidth: Int,
+    buttonHeight: Int,
+    onClickNext: () -> Unit,
+    onClickPrevious: () -> Unit
+) {
+
+    Column(
+        modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
+        Surface(
+            color = Color(0xFFF5F5F5), // gris clarito casi blanco
+            shape = RoundedCornerShape(12.dp),
+            shadowElevation = 4.dp,
             modifier = Modifier
-                .padding(12.dp)
+                .width(columnWith.dp)
+                .height(columnHeight.dp)
+                .padding(horizontal = 16.dp)
         ) {
-            Text(
-                text = stringResource(obra.artista),
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
-                fontFamily = FontFamily.Cursive
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = "Imagen ${index + 1}/${obras.size}",
-                fontSize = 22.sp,
-                fontWeight = FontWeight.Bold,
-                fontFamily = FontFamily.Cursive
-            )
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.padding(12.dp)
+            ) {
+                Text(
+                    text = stringResource(obra.artista),
+                    fontSize = texSize1.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    fontFamily = FontFamily.Cursive
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = "Imagen ${index + 1}/${obras.size}",
+                    fontSize = textSize2.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = FontFamily.Cursive
+                )
+            }
         }
+        Spacer(modifier = Modifier.height(20.dp))
+        CrearBotones(
+            texSize = texSize1,
+            buttonHeight = buttonHeight,
+            buttonWidth = buttonWidth,
+            onClickNext = onClickNext,
+            onClickPrevious = onClickPrevious
+        )
+
+
     }
+
 }
 //FIN_2 (David)
+
+// INICIO (Rubén)
+@Composable
+fun CrearBotones(
+    modifier: Modifier = Modifier,
+    buttonWidth: Int,
+    buttonHeight: Int,
+    texSize: Int,
+    onClickNext: () -> Unit,
+    onClickPrevious: () -> Unit
+) {
+
+    Box(modifier = modifier.fillMaxWidth()) {
+        Button(
+            modifier = Modifier
+                .padding(start = 30.dp)
+                .width(buttonWidth.dp)
+                .height(buttonHeight.dp)
+                .align(Alignment.CenterStart), onClick = onClickPrevious
+        ) {
+            Text(
+                text = "Previouse", fontSize = texSize.sp
+            )
+
+        }
+
+        Button(
+            modifier = Modifier
+                .padding(end = 30.dp)
+                .width(buttonWidth.dp)
+                .height(buttonHeight.dp)
+                .align(Alignment.BottomEnd), onClick = onClickNext
+        ) {
+            Text(
+                text = "Next", fontSize = texSize.sp
+
+            )
+
+        }
+    }
+
+}
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     P1_ArtSpace_RubenMartinAndrade_HugodePabloLopez_DavidLopezTapiaTheme {
-        crearInterfaz()
+        CrearInterfaz()
     }
 }
